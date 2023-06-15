ad283cad8792c993c601c3d6f422c36138935666
mathipeeg <math_dahl@hotmail.com>
2023-06-15 16:51:08 +0200

    fixed timer with the data

--- angular/src/app/services/specimen-graph.service.ts
+++ angular/src/app/services/specimen-graph.service.ts
@@ -1,13 +1,14 @@
 import {Injectable} from '@angular/core';
-import {OidcSecurityService} from "angular-auth-oidc-client";
-import {HttpClient, HttpResponse} from "@angular/common/http";
-import {catchError, Observable, of, switchMap} from "rxjs";
+import {OidcSecurityService} from 'angular-auth-oidc-client';
+import {HttpClient, HttpResponse} from '@angular/common/http';
+import {catchError, Observable, of, switchMap, timer} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class SpecimenGraphService {
   baseUrl = '/api/v1/graphdata';
+  fiveMinutes = 5 * 60 * 1000;
 
   constructor(
     public oidcSecurityService: OidcSecurityService
@@ -15,36 +16,51 @@ export class SpecimenGraphService {
   ) { }
 
   specimenDataWeek$: Observable<HttpResponse<any> | undefined>
-    = this.oidcSecurityService.getAccessToken()
+    = timer(0, this.fiveMinutes)
     .pipe(
-      switchMap((token) => {
-        return this.http.get(`${this.baseUrl}/WEEK`, {headers: {'Authorization': 'Bearer ' + token}, observe: 'response'})
-          .pipe(
-            catchError(this.handleError(`get ${this.baseUrl}/daily/WEEK`, undefined))
-          );
-      })
+      switchMap(() =>
+      this.oidcSecurityService.getAccessToken()
+        .pipe(
+          switchMap((token) => {
+            return this.http.get(`${this.baseUrl}/WEEK`, {headers: {'Authorization': 'Bearer ' + token}, observe: 'response'})
+              .pipe(
+                catchError(this.handleError(`get ${this.baseUrl}/WEEK`, undefined))
+              );
+          })
+        )
+      )
     );
 
   specimenDataMonth$: Observable<HttpResponse<any> | undefined>
-    = this.oidcSecurityService.getAccessToken()
+    = timer(0, this.fiveMinutes)
     .pipe(
-      switchMap((token) => {
-        return this.http.get(`${this.baseUrl}/MONTH`, {headers: {'Authorization': 'Bearer ' + token}, observe: 'response'})
+      switchMap(() =>
+        this.oidcSecurityService.getAccessToken()
           .pipe(
-            catchError(this.handleError(`get ${this.baseUrl}/daily/MONTH`, undefined))
-          );
-      })
+            switchMap((token) => {
+              return this.http.get(`${this.baseUrl}/MONTH`, {headers: {'Authorization': 'Bearer ' + token}, observe: 'response'})
+                .pipe(
+                  catchError(this.handleError(`get ${this.baseUrl}/MONTH`, undefined))
+                );
+            })
+          )
+      )
     );
 
   specimenDataYear$: Observable<HttpResponse<any> | undefined>
-    = this.oidcSecurityService.getAccessToken()
+    = timer(0, this.fiveMinutes)
     .pipe(
-      switchMap((token) => {
-        return this.http.get(`${this.baseUrl}/YEAR`, {headers: {'Authorization': 'Bearer ' + token}, observe: 'response'})
-          .pipe(
-            catchError(this.handleError(`get ${this.baseUrl}/year`, undefined))
-          );
-      })
+      switchMap(() =>
+      this.oidcSecurityService.getAccessToken()
+        .pipe(
+          switchMap((token) => {
+            return this.http.get(`${this.baseUrl}/YEAR`, {headers: {'Authorization': 'Bearer ' + token}, observe: 'response'})
+              .pipe(
+                catchError(this.handleError(`get ${this.baseUrl}/YEAR`, undefined))
+              );
+          })
+        )
+      )
     );
 
   getSpecimenDataCustom(view: string, start: number, end: number): Observable<HttpResponse<any> | undefined> {
