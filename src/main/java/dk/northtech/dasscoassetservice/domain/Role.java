75540326173203e1b6a06b0dbb82228179c74948
Thomas <Thomas@northtech.dk>
2023-07-04 19:02:54 +0200

    Missing updates from last request

--- src/main/java/dk/northtech/dasscoassetservice/domain/Role.java
+++ src/main/java/dk/northtech/dasscoassetservice/domain/Role.java
@@ -1,5 +1,13 @@
 package dk.northtech.dasscoassetservice.domain;
 
 public enum Role {
-    USER, ADMIN, SERVICE_USER
+    USER(SecurityRoles.USER)
+    , ADMIN(SecurityRoles.ADMIN)
+    , SERVICE_USER(SecurityRoles.SERVICE)
+    , DEVELOPER(SecurityRoles.DEVELOPER);
+
+    public final String roleName;
+    Role(String role) {
+        this.roleName = role;
+    }
 }
