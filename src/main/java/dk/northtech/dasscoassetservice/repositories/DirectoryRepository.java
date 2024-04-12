b92dc4371ac4d2f49a92d19ae67dc6245f822f84
Thomas <Thomas@northtech.dk>
2024-04-12 14:46:18 +0200

    improvement:  move comment to correct method

--- src/main/java/dk/northtech/dasscoassetservice/repositories/DirectoryRepository.java
+++ src/main/java/dk/northtech/dasscoassetservice/repositories/DirectoryRepository.java
@@ -12,13 +12,13 @@ import java.util.List;
 
 public interface DirectoryRepository {
 
-    //Find a directory by assetGuid. Should only return one as only single asset directories will have write access.
     @SqlQuery("""
         SELECT d.*, sa.asset_guid FROM dassco_file_proxy.directories d 
             LEFT JOIN dassco_file_proxy.shared_assets sa ON sa.directory_id = d.directory_id 
         WHERE d.access = 'WRITE'::dassco_file_proxy.access_type 
 """)
     List<Directory> getWriteableDirectories();
+    //Find a directory by assetGuid. Should only return one as only single asset directories will have write access.
     @SqlQuery("""
         SELECT d.*, sa.asset_guid FROM dassco_file_proxy.directories d 
             LEFT JOIN dassco_file_proxy.shared_assets sa ON sa.directory_id = d.directory_id 
