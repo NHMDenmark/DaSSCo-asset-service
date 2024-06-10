ffa76b69895da673323f563147f6fef3d7da08ff
Nicolas <nicolas@northtech.dk>
2024-06-10 15:27:29 +0200

    fix docs not working

--- angular/src/assets/frontend-properties.js
+++ angular/src/assets/frontend-properties.js
@@ -1,6 +1,6 @@
 var frontendProperties = {
   authenticationUrl: 'http://localhost:8083/realms/dassco',
-  rootUrl: 'http://localhost:5456',
-  fileProxyRootUrl: 'http://localhost:43654',
+  rootUrl: 'http://localhost:8084',
+  fileProxyRootUrl: 'http://localhost:8080',
   clientId: 'dassco-asset-service'
 }
