2f793ac7af67bc65141b2dd489084b4af1d4bf9d
Nicolas <nicolas@northtech.dk>
2024-07-08 14:30:29 +0200

    add pipeline cache

--- src/main/java/dk/northtech/dasscoassetservice/cache/CacheInitializer.java
+++ src/main/java/dk/northtech/dasscoassetservice/cache/CacheInitializer.java
@@ -2,8 +2,11 @@ package dk.northtech.dasscoassetservice.cache;
 
 import dk.northtech.dasscoassetservice.domain.Collection;
 import dk.northtech.dasscoassetservice.domain.Institution;
+import dk.northtech.dasscoassetservice.domain.Pipeline;
 import dk.northtech.dasscoassetservice.repositories.CollectionRepository;
 import dk.northtech.dasscoassetservice.repositories.InstitutionRepository;
+import dk.northtech.dasscoassetservice.repositories.PipelineRepository;
+import dk.northtech.dasscoassetservice.services.PipelineService;
 import org.springframework.context.ApplicationListener;
 import jakarta.inject.Inject;
 import org.springframework.context.event.ContextRefreshedEvent;
@@ -18,15 +21,20 @@ public class CacheInitializer implements ApplicationListener<ContextRefreshedEve
     private InstitutionRepository institutionRepository;
     private CollectionCache collectionCache;
     private CollectionRepository collectionRepository;
+    private PipelineCache pipelineCache;
+    private PipelineRepository pipelineRepository;
     private boolean initialized = false;
 
     @Inject
     public CacheInitializer(InstitutionCache institutionCache, InstitutionRepository institutionRepository,
-                            CollectionCache collectionCache, CollectionRepository collectionRepository){
+                            CollectionCache collectionCache, CollectionRepository collectionRepository,
+                            PipelineCache pipelineCache, PipelineRepository pipelineRepository){
         this.institutionCache = institutionCache;
         this.institutionRepository = institutionRepository;
         this.collectionCache = collectionCache;
         this.collectionRepository = collectionRepository;
+        this.pipelineCache = pipelineCache;
+        this.pipelineRepository = pipelineRepository;
     }
 
     @Override
@@ -40,6 +48,12 @@ public class CacheInitializer implements ApplicationListener<ContextRefreshedEve
                     if (!collectionList.isEmpty()){
                         for (Collection collection : collectionList){
                             this.collectionCache.putCollectionInCache(institution.name(), collection.name(), collection);
+                        }
+                    }
+                    List<Pipeline> pipelineList = pipelineRepository.listPipelines(institution);
+                    if (!pipelineList.isEmpty()){
+                        for (Pipeline pipeline : pipelineList){
+                            this.pipelineCache.putPipelineInCache(pipeline);
                         }
                     }
                 }
