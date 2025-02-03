960fd1181cb5ecf2a38a057d85b08ff5b554fd96
Thomas <Thomas@northtech.dk>
2025-02-03 10:55:31 +0100

    Chore: version 1.7.5

--- src/main/java/dk/northtech/dasscoassetservice/domain/Institution.java
+++ src/main/java/dk/northtech/dasscoassetservice/domain/Institution.java
@@ -7,14 +7,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public record Institution(
-        @Schema(description = "The name of the institution which owns and digitised the specimen", example = "test-institution")
-        String name
-        ,
-        @ArraySchema(schema = @Schema( description = "The roles you need to access assets within this institution",implementation = Role.class))
-        List<Role> roleRestriction) {
+    @Schema(description = "The name of the institution which owns and digitised the specimen", example = "test-institution")
+    String name
+    ,
+    @ArraySchema(schema = @Schema(description = "The roles you need to access assets within this institution", implementation = Role.class))
+    List<Role> roleRestriction) {
 
-
-        public Institution(String name) {
-                this(name, new ArrayList<>());
-        }
+    public Institution(String name) {
+        this(name, new ArrayList<>());
+    }
 }
