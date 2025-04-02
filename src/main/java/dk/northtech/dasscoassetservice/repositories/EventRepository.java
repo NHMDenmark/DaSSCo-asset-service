01704b255371255b677a89db507cfc6c3073bb16
Thomas <Thomas@northtech.dk>
2025-04-02 15:38:04 +0200

    Feature: merge user and digitiser tables

---
+++ src/main/java/dk/northtech/dasscoassetservice/repositories/EventRepository.java
@@ -0,0 +1,9 @@
+package dk.northtech.dasscoassetservice.repositories;
+
+import org.jdbi.v3.sqlobject.SqlObject;
+import org.jdbi.v3.sqlobject.statement.SqlUpdate;
+
+public interface EventRepository extends SqlObject {
+
+
+}
