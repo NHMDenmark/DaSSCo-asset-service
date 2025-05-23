f4bc462ebe3b155c09c85c6fc9f0c04fd792ca64
Thomas <Thomas@northtech.dk>
2025-05-23 13:45:02 +0200

    Bugfix: Fix institution name not being set by jdbi constructor

--- src/main/java/dk/northtech/dasscoassetservice/domain/InstitutionRoleRestriction.java
+++ src/main/java/dk/northtech/dasscoassetservice/domain/InstitutionRoleRestriction.java
@@ -5,8 +5,6 @@ import org.jdbi.v3.core.mapper.reflect.JdbiConstructor;
 public record InstitutionRoleRestriction(String institution_name, Role role) {
 
     @JdbiConstructor
-    public InstitutionRoleRestriction(String institution_name, Role role) {
-        this.institution_name = institution_name();
-        this.role = role;
+    public InstitutionRoleRestriction {
     }
 }
