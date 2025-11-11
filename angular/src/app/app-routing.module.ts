8c27c53b899361a4f557cb034b0755392882efb6
Magnus Granno <magnus@northtech.dk>
2025-11-11 13:28:51 +0100

    removes bulk update page

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -5,7 +5,6 @@ import {AuthenticatedGuard} from './guards/authenticated.guard';
 import {DocsComponent} from './components/docs/docs.component';
 import {GraphComponent} from './components/graph/graph.component';
 import {StatisticsComponent} from './components/statistics/statistics.component';
-import {BulkUpdateComponent} from './components/bulk-update/bulk-update.component';
 import {QueriesComponent} from './components/queries/queries.component';
 import {DetailedViewComponent} from './components/detailed-view/detailed-view.component';
 import {AssetGroupsComponent} from './components/asset-groups/asset-groups.component';
@@ -17,7 +16,6 @@ const routes: Routes = [
   {path: 'graphs', component: GraphComponent, canActivate: [AuthenticatedGuard]},
   {path: 'user', component: UserComponent, canActivate: [AuthenticatedGuard]},
   {path: 'docs', component: DocsComponent, canActivate: [AuthenticatedGuard]},
-  {path: 'bulk-update', component: BulkUpdateComponent, canActivate: [AuthenticatedGuard]},
   {path: 'queries', component: QueriesComponent, canActivate: [AuthenticatedGuard]},
   {path: 'asset-groups', component: AssetGroupsComponent, canActivate: [AuthenticatedGuard]},
   {path: 'detailed-view/:asset_guid', component: DetailedViewComponent, canActivate: [AuthenticatedGuard]},
